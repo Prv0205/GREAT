0c093744a092532e8d16e59bcc2b14896b0b9efa
Prv0205 <prv.icfoss@gmail.com>
2026-08-18 23:39:50 +0100

    Update Next.js configuration for export and base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/GREAT',
+  assetPrefix: '/GREAT/',
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
