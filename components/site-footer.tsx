f8813ed40f0136818c46ef5b459f51fd7b4dc1b4
Prv0205 <prv.icfoss@gmail.com>
2026-08-19 02:49:24 +0100

    Update logo source path in site-footer component

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -66,7 +66,7 @@ export function SiteFooter() {
               className="inline-flex items-center rounded-xl bg-white px-3 py-2"
             >
               <Image
-                src="/GREAT/logo.png"
+                src="/logo.png"
                 alt="GREAT Galway Regional Education and Tutorial Center"
                 width={150}
                 height={90}
